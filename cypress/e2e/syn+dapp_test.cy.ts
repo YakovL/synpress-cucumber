cd935ce0779eed6cb5ebf6ac307feaad31a9e340
Yakov Litvin <Yakov.A.Litvin@gmail.com>
2023-06-04 20:26:56 +0300

    add .feature and .cy.ts steps

---
+++ cypress/e2e/syn+dapp_test.cy.ts
@@ -0,0 +1,23 @@
+import { Given, When, Then } from '@badeball/cypress-cucumber-preprocessor';
+
+// Problem 1: tests fail with "Cypress detected that you returned a promise from a command while also invoking one or more cy commands in that promise."
+// Problem 2: .feature doesn't suggest boilerplates unlike Cucumber. Should we use cli to get that?
+
+Given("we're on the test dapp page", () => {
+  cy.visit('https://metamask.github.io/test-dapp/');
+});
+
+When('we press connect', function () {
+  cy.get('#connectButton').click();
+});
+
+When('we accept notification', function () {
+  cy.acceptMetamaskAccess().should('be.true');
+});
+
+Then(
+  'below the asset image there should be a toggle wishlist button',
+  function () {
+    cy.get('#connectButton').should('have.text', 'Connected');
+  }
+);
---
+++ cypress/e2e/syn+dapp_test.feature
@@ -0,0 +1,10 @@
+Feature: Test of Synpress and Metamask with a DApp from a tutorial
+
+    Trivial scenario for a test app
+
+        Scenario: Open, connect, confirm
+
+            Given we're on the test dapp page
+            When we press connect
+            When we accept notification
+            Then the button should say Connected
