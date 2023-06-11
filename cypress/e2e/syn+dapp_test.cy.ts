70c60135b883b7090f181adffbe14f94f9d03483
Yakov Litvin <Yakov.A.Litvin@gmail.com>
2023-06-11 18:28:40 +0300

    docs: update comments describing problems with the current setup

--- cypress/e2e/syn+dapp_test.cy.ts
+++ cypress/e2e/syn+dapp_test.cy.ts
@@ -1,7 +1,10 @@
-import { Given, When, Then } from '@badeball/cypress-cucumber-preprocessor';
-
-// Problem 1: tests fail with "Cypress detected that you returned a promise from a command while also invoking one or more cy commands in that promise."
+// Problem 1:
+//  while  npm run test:e2e  works (cypress run),  npm run test  (cypress open → ...) fails with
+//  "Cypress detected that you returned a promise from a command while also invoking one or more cy commands in that promise."
+//  This complicates writing tests as with  cypress open  we have hot reloading and fast feedback loop when writing tests.
 // Problem 2: .feature doesn't suggest boilerplates unlike Cucumber. Should we use cli to get that?
+
+import { Given, When, Then } from '@badeball/cypress-cucumber-preprocessor';
 
 Given("we're on the test dapp page", () => {
   cy.visit('https://metamask.github.io/test-dapp/');
